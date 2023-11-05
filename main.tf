4795e406a0b473e1c284a1636c966469556b2db4
Diogo Magalhães <d.magalhaes@ua.pt>
2023-11-05 14:44:09 +0000

    NOTE-68 terraform main script

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  access_key  = local.envs["AWS_ACCESS_KEY_ID"]
+  secret_key  = local.envs["AWS_SECRET_ACCESS_KEY"]
+  token       = try(local.envs["AWS_SESSION_TOKEN"], null)
+  region      = local.envs["AWS_DEFAULT_REGION"]
+
+}
+
+module "noteally_vpc"{
+  source = "./aws-network"
+}
