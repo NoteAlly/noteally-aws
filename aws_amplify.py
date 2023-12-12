885ee2be08046c4841e1558ea065c5173aba44fb
Pedro Rodrigues <pedrofrodrigues4@gmail.com>
2023-12-12 19:35:11 +0000

    Fix region env var format

--- aws_amplify.py
+++ aws_amplify.py
@@ -14,7 +14,7 @@ if __name__=='__main__':
         aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
     )
 
-    region = os.environ.get('AWS_REGION_NAME'),
+    region = os.environ.get('AWS_REGION_NAME')
     branch = os.environ.get('FRONTEND_BRANCH')
 
     with open('terraform.tfstate', 'rt') as f:
@@ -34,9 +34,9 @@ if __name__=='__main__':
             
             environmentVariables = {
                 "VITE_API_URL": f"{api_gateway_url}dev",
-                "VITE_AUTH_LOGIN": f"https://{cognito_client_domain}.auth.{region}.amazoncognito.com/login?client_id={cognito_client_id}&response_type=token&scope=email+openid+profile&redirect_uri=https://{branch}.{amplify_domain}/login?",
-                "VITE_AUTH_REGISTER": f"https://{cognito_client_domain}.auth.{region}.amazoncognito.com/signup?client_id={cognito_client_id}&response_type=token&scope=email+openid+profile&redirect_uri=https://{branch}.{amplify_domain}/login?",
-                "VITE_AUTH_LOGOUT": f"https://{cognito_client_domain}.auth.{region}.amazoncognito.com/logout?client_id={cognito_client_id}&logout_uri=https://{branch}.{amplify_domain}",
+                "VITE_AUTH_LOGIN": f"https://{cognito_client_domain}.auth.{region[0]}.amazoncognito.com/login?client_id={cognito_client_id}&response_type=token&scope=email+openid+profile&redirect_uri=https://{branch}.{amplify_domain}/login?",
+                "VITE_AUTH_REGISTER": f"https://{cognito_client_domain}.auth.{region[0]}.amazoncognito.com/signup?client_id={cognito_client_id}&response_type=token&scope=email+openid+profile&redirect_uri=https://{branch}.{amplify_domain}/login?",
+                "VITE_AUTH_LOGOUT": f"https://{cognito_client_domain}.auth.{region[0]}.amazoncognito.com/logout?client_id={cognito_client_id}&logout_uri=https://{branch}.{amplify_domain}",
             }
         )
     
